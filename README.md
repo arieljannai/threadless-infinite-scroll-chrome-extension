ad1e9807363b5a4ed8d0c5a25868fa5c8422e592
Ariel Jannai <arieljannai@gmail.com>
2016-09-18 09:32:28 +0300

    convert greasemonkey to chrome extension

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Threadless](https://www.threadless.com) Infinite Scroll
+Replace the default pagination with infinite scroll.<BR>
+Makes it much easier to browse tons of products fast!
+
+![](http://i.imgur.com/NH1zQtD.gif)
+
+### Suggestions, bugs etc.
+At [GitHub](https://github.com/arieljannai/threadless-infinite-scroll-chrome-extension/issues)
---
+++ js/content.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var infiniteLoadingGif = "<div class='infinite-loading' style='text-align:center; margin:0 auto; width:50%'><img src='https://getmocky.com/images/big_preloader.gif'></img></div>";
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+var back_top = $('.back_top');
+var pathname = document.location.pathname;
+var pageMatch = pathname.match(/page,(\d+)/);
+var currPage = pageMatch ? pageMatch[1] : 1;
+var requestNext = true;
+pathname = pathname.replace(/\/page,\d+/, '') + '/page,' + currPage;
+
+function getPagePath(pageNum) {
+    return pathname.replace(/page,\d+/, 'page,' + pageNum);
+}
+
+function appendPageItems(pageNum) {
+    console.log('loading page: ', pageNum);
+    var msg = '';
+    $('<div>').load(getPagePath(pageNum) + ' #catalog_products', function(html, status) {
+        console.log('status: ', status);
+        if (status !== 'success') {
+            requestNext = false;
+            msg = 'Oops. There was an error while loading the next page items (page ' + pageNum + ').';
+            console.error(msg);
+            $('<div style="text-align:center; color:red">' + msg + '</div>').insertAfter('#catalog_products');
+            $('div .catalog_browsing').show();
+        } else if (html.indexOf('class="no-results"') > -1) {
+            console.log('not loaded: ', pageNum);
+            msg = 'No more items :(';
+            console.log(msg);
+            $('<div style="text-align:center">' + msg + '</div>').insertAfter('#catalog_products');
+        } else {
+            $('#catalog_products').append($(this));
+            requestNext = true;
+            console.log('loaded:', pageNum);
+        }
+
+        $('.infinite-loading').hide();
+    });
+}
+
+$('div .catalog_browsing').hide();
+$(infiniteLoadingGif).insertBefore('div .catalog_browsing');
+$('.infinite-loading').hide();
+
+$(window).scroll(function() {
+    if (requestNext && isScrolledIntoView(back_top)) {
+        $('.infinite-loading').show();
+        requestNext = false;
+        appendPageItems(++currPage);
+    }
+});
